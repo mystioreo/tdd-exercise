527b5c8e6aaa10918020eb0b1e383ab91e684244
Daniela Di Iorio <danielad@gmail.com>
2018-08-14 14:07:51 -0700

    wrote blackjack_score.rb to pass all tests in blackjack_score_spec.rb

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,37 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
+
+  if (!hand.all? {|card| VALID_CARDS.include? card})
+    raise ArgumentError
+  end
+
+  hand.map! { |card|
+    if card.to_i >= 2 && card.to_i <= 10
+      card
+    elsif (card == "King" || card == "Queen" || card == "Jack")
+      10
+    elsif card == 1   #at first, turn every Ace into 11
+      11
+    end
+  }
+
+  score = hand.sum
+
+  if hand.count(11) > 1
+    score -= (10 * hand.count(11)-1)
+  end
+
+  if score > 21 && hand.count(11) >= 1
+    score -= 10
+  end
+ puts score
+  if score > 21
+    raise ArgumentError
+  end
+
+  return score
 
 end
--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -3,6 +3,7 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/pride'
+require 'minitest/skip_dsl'
 
 require_relative '../lib/blackjack_score'
 
@@ -18,26 +19,97 @@ describe 'Blackjac Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
+    # Arrange
+    hand = ["Queen", 7, 2]
+
+    #Act
+    score = blackjack_score(hand)
+
+    #Assert
+    expect(score).must_equal 19
+
 
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+    #Arrange
+    hand = [1, "Queen"]
+
+    #Act
+    score = blackjack_score(hand)
+
+    #Assert
+    expect(score).must_equal 21
+
+    #re-Arrange
+    hand = [2, 1, 3, 4]
+
+    #Act
+    score = blackjack_score(hand)
+
+    #Assert
+    expect(score).must_equal 20
+
+
 
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
 
+    #Arrange
+    hand = [1, "Queen", "King"]
+
+    #Act
+    score = blackjack_score(hand)
+
+    #Assert
+    expect(score).must_equal 21
+
+    #re-Arrange
+    hand = [10, 1, 3, 4]
+
+    #Act
+    score = blackjack_score(hand)
+
+    #Assert
+    expect(score).must_equal 18
+
+
+
   end
 
   it 'raises an ArgumentError for invalid cards' do
 
+    #Arrange
+    hand = [7, "BadCard", 10]
+
+    #Act-Assert
+    expect {
+      score = blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    #re-Arrange
+    hand = [7, 2, 11]
+
+    #Act-Assert
+    expect {
+      score = blackjack_score(hand)
+    }.must_raise ArgumentError
   end
 
   it 'raises an ArgumentError for scores over 21' do
+    #Arrange
+    hand = [7, "Queen", 5]
+
+    #Act-Assert
+    expect {
+      score = blackjack_score(hand)
+    }.must_raise ArgumentError
 
   end
 end
